8124afc81562c70e153d8e9a164f97d3658d26bf
rscibelli <scibellrob@gmail.com>
2024-01-20 13:29:02 -0500

    Set up project

---
+++ src/components/Dump.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const Dump = () => {
+  const [newPost, setNewPost] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const handlePostChange = (e) => {
+    setNewPost(e.target.value);
+  };
+
+  const handlePostSubmit = () => {
+    if (newPost.trim() !== '') {
+      setPosts([...posts, newPost]);
+      setNewPost('');
+    }
+  };
+
+  return (
+    <div className="blog p-2 mt-2">
+      <div className='mx-5'>
+        <h3 className='text-start'>What's on your mind?</h3>
+        <textarea className="form-control bg-light" rows="3" onChange={handlePostChange}></textarea>
+        <div className='text-end'>
+            <button className="btn btn-primary mt-1" onClick={handlePostSubmit}>Submit</button>
+        </div>
+      </div>
+
+      <hr className='w-75 mx-auto' />
+
+      <div className="previous-posts">
+        <h4>Previous Blog Posts</h4>
+        {posts.map((post, index) => (
+          <div key={index} className="blog-post">
+            <p className="blog-post-content">{post}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dump;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h1 className="text-start">BrainDump</h1>
+    </div>
+  );
+};
+
+export default Header;

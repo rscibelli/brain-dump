57278c3377d24b6149562a8f3a250c65495dce60
rscibelli <scibellrob@gmail.com>
2024-01-23 17:02:27 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@ export $( grep -vE "^(#.*|\s*)$" .env )
 sshpass -p ${ssh_pass} ssh pi@10.0.0.251 << EOF
     cd repositories/brain-dump
     git pull
-    cd /backend
-    env GOOS=linux GOARCH=arm GOARM=7 go build
-    docker compose up -d --force-restart
+    docker compose up -d --force-recreate
     docker container ls
 EOF

3f2828f6cf7b0db24dc37026fc9a75acd7f03489
rscibelli <scibellrob@gmail.com>
2024-02-24 13:04:31 -0500

    change nginx.conf

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 export $( grep -vE "^(#.*|\s*)$" .env )
 
-sshpass -p ${ssh_pass} ssh pi@10.0.0.251 << EOF
+sshpass -p ${ssh_pass} ssh -p 2332 pi@10.0.0.251 << EOF
     cd repositories/brain-dump
     git pull
     docker compose up -d --force-recreate

529b4e4caee980c8a3d85364ba0d26e9b9e95c3c
rscibelli <scibellrob@gmail.com>
2024-02-25 20:11:22 -0500

    spacing

--- react-app/src/components/Card.jsx
+++ react-app/src/components/Card.jsx
@@ -6,10 +6,10 @@ const Card = ({ post }) => {
   return (
     <div className="card">
         <div className="card-header bg-secondary text-white">
-            <h5 className="card-title mb-0">{title}</h5>
+            <h5 className="card-title mb-3">{title}</h5>
             <div className="d-flex justify-content-between align-items-center">
-            <h6 className="card-subtitle mb-1">{name}</h6>
-            <span className="card-date">{date}</span>
+              <h6 className="card-subtitle mb-1">{name}</h6>
+              <span className="card-date">{date}</span>
             </div>
         </div>
         <div className="card-body text-start">
